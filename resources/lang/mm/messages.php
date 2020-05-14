e5d69095f9292d37078bbea7ce89dc596e78ea32
Wai Yan Htet <whispx93@gmail.com>
2020-05-14 16:53:52 +0630

    add translation

--- resources/lang/mm/messages.php
+++ resources/lang/mm/messages.php
@@ -1,11 +1,16 @@
 <?php
 
 return [
-    // Backend Messages
+    /** backend/auth/login.blade.php */
     'login_box_message' => 'အက်ဒမင် အကောင့်၀င်ရန်',
     'email' => 'အီးမေးလ်လိပ်စာ',
     'password' => 'စကားဝှက်',
-    'login' => 'ဝင်မည်'
-
-    // Frontend Messages
+    'login' => 'ဝင်မည်',
+    /** backend/partials/commons/_navbar.blade.php */
+    'welcome' => 'ကြိုဆိုပါတယ်',
+    'logout' => 'ထွက်မည်',
+    /** backend/partials/commons/_sidbar.blade.php */
+    'dashboard' => 'ပင်မ',
+    /** backend/partials/commons/_bottom.blade.php */
+    /** backend/partials/commons/_content_header.blade.php */
 ];
